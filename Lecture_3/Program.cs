297749ea3ccddb32e02039860b7cca96b2d5fb75
Smorodyan <smorodyan@gmail.com>
2023-01-17 13:36:15 +0300

    Changes for Lecture_3 added

--- Lecture_3/Program.cs
+++ Lecture_3/Program.cs
@@ -1,10 +1,16 @@
-﻿// int Method()
+﻿// ЗАДАЧА_1:
+
+// int Method()
 // {
 //     return DateTime.Now.Year;
 // }
 
 // //int year = Method();
 // Console.Write(Method());
+
+
+// ЗАДАЧА_2:
+
 
 /*
 void Method()
@@ -24,6 +30,10 @@ void Method()
 Method();
 */
 
+
+// ЗАДАЧА_3:
+
+/*
 string Replacement(string text, char oldValue, char newValue, char oldK, char newK, char oldC, char newC)
 {
     string reuslt = String.Empty;
@@ -45,4 +55,206 @@ string text = "— Я думаю, — сказал князь, улыбаясь,
         + "Вы так красноречивы. Вы дадите мне чаю?";
 
 Console.Write(Replacement(text, ' ', '_', 'к', 'К', 'с', 'С'));
+*/
+
+
+
+// ЗАДАЧА_4
+
+
+// {6, 2, 5, 7, 3, 9, 5}
+/*
+void Massiv(int[] array)
+{
+    for(int i = 0; i <= array.Length - 2; ++i)
+    {
+        for(int j = 1; j <= array.Length - 1; ++j)
+        if(array[i] > array[j])
+        {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+            Console.WriteLine(array[i]);
+        }
+    }
+}
+
+
+
+//int[] array = new int[10];
+int[] array = {4, 2, 7, 9, 1, 0, 5, 6};
+//Console.Write(array[0]);
+
+Massiv(array);
+*/
+
+
+
+// ЗАДАЧА_5:
+
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int[] ConvertArray(int[] array)
+{
+    int i = 0;
+        
+    while(i < array.Length)
+    {
+        int j = i + 1;
+        while(j < array.Length)
+        {
+            int max = array[i];
+            if(max <= array[j])
+            {
+                max = array[j];
+                array[j] = array[i];
+                array[i] = max;
+                j++;
+            }
+            else j++;
+        }
+        ++i;
+    }    
+    return array;
+        
+}
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(length, min, max);
+ShowArray(newArray);
+int[] convertedArray = ConvertArray(newArray);
+ShowArray(convertedArray);
+*/
+
+
+// ЗАДАЧА_6:
+
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+//int[] ConvertArray(int[] array)
+void ConvertArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[i] < array[j])
+            {
+                int max = array[j];
+                array[j] = array[i];
+                array[i] = max;
+            }
+        }
+    }
+  //  return array;
+        
+}
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(length, min, max);
+ShowArray(newArray);
+// int[] convertedArray = ConvertArray(newArray);
+// ShowArray(convertedArray);
+ConvertArray(newArray);
+ShowArray(newArray);
+*/
+
+
+
+// ЗАДАЧА_7
+
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+void ConvertedArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[i] > array[j])
+            {
+                int max = array[i];
+                array[i] = array[j];
+                array[j] = max;
+            }
+        }
+    }
+}
+
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = (CreateRandomArray(length, min, max));
+ShowArray(newArray);
+ConvertedArray(newArray);
+ShowArray(newArray);
 
